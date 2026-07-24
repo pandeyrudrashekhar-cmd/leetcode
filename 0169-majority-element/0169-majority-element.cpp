3c6b8d95bbfdb6a8ebfcd7720fe3d59629fca3a6
Rudra shekhar Pandey <pandeyrudrashekhar@gmail.com>
2026-07-24 12:22:38 +0530

    Time: 0 ms (100%), Space: 28.1 MB (70.33%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int n=nums.size();
-        map<int,int>freq;
-        for(int i=0; i<n; i++){
-            freq[nums[i]]++;
-        }
-        for(auto it: freq){
-            if(it.second>n/2){
-                return it.first;
+        //boyer more algo
+        int candidate=0;
+        int count=0;
+        for(int i=0; i<nums.size(); i++){
+            //if count becomes zero then choose new candidate
+            if(count==0) candidate=nums[i];
+            //if current element matches camdidate then increase count
+            if(nums[i]==candidate) count++;
+            else{
+                count--;
             }
         }
-        return -1;
+        return candidate;
     }
 };
