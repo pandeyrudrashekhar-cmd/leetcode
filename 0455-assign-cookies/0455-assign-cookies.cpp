f07fe61f7629672d0e0c7b73a00e8f9438678854
Rudra shekhar Pandey <pandeyrudrashekhar@gmail.com>
2026-08-09 17:32:04 +0530

    Time: 9 ms (49.8%), Space: 44.7 MB (97.93%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -3,13 +3,13 @@ public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
         sort(g.begin(),g.end());
         sort(s.begin(),s.end());
-        int child=0,cookie=0;
-        //try assuming cookies
+
+        int child=0;
+        int cookie=0;
+        //try to assign cookies
         while(child<g.size() && cookie<s.size()){
-            //if curr cookie can satisfy current child
-            if(s[cookie]>=g[child]){
-                child++;//move to next child
-            }
+            if(s[cookie]>=g[child]) child++;
+            //move to next in both case either cookie was assigned or cookie was too small and discarded
             cookie++;
         }
         return child;
