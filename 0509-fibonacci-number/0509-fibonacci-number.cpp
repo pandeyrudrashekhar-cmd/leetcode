e75fbfb4d5ec0aa6cf2d0b49f1fba696f0e3b3a5
Rudra shekhar Pandey <pandeyrudrashekhar@gmail.com>
2026-08-09 09:33:26 +0530

    Time: 3 ms (64.76%), Space: 8.2 MB (17.08%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,12 +1,16 @@
 class Solution {
 public:
-int fibo(int n, vector<int>& dp){
-    if(n<=1) return n;
+int fibo(int n, vector<int>&dp){
+    //base case
+    if(n<2) return n;
+    //dp condition base case
     if(dp[n]!=-1) return dp[n];
-    return dp[n]=fibo(n-1,dp)+fibo(n-2,dp);
+     dp[n]=fibo(n-1,dp)+fibo(n-2,dp);
+     return dp[n];
 }
     int fib(int n) {
-       vector<int>dp(n+1,-1);
-     return fibo(n,dp);
+        
+        vector<int>dp(n+1,-1);
+        return fibo(n,dp);
     }
 };
