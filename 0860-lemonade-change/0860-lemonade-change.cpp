47e4991ae275063deab371df880cb02ccaaae274
Rudra shekhar Pandey <pandeyrudrashekhar@gmail.com>
2026-09-01 01:30:29 +0530

    Time: 0 ms (100%), Space: 87.2 MB (44.9%) - LeetHub

--- 0860-lemonade-change/0860-lemonade-change.cpp
+++ 0860-lemonade-change/0860-lemonade-change.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
     bool lemonadeChange(vector<int>& bills) {
-        int five=0;
-        int ten=0;
-        for( int bill : bills){
-            if(bill==5) five++;
-            else if(bill==10){
-                //need one five as change
-                if(five==0) return false;
+        int five = 0, ten = 0;
+        for (int bill : bills) {
+            if (bill == 5) {
+                five++;
+            } else if (bill == 10) {
+                if (five == 0) return false;
                 five--;
                 ten++;
-            }
-            else{//customer pay 20
-                //greedy approach:- prefer giving one 10 and one 5
-                if(ten>=1 && five>=1){
+            } else { // bill == 20
+                if (ten > 0 && five > 0) {
                     ten--;
                     five--;
+                } else if (five >= 3) {
+                    five -= 3;
+                } else {
+                    return false;
                 }
-                //otherwise give 3 five bills
-                else if(five>=3) five-=3;
-                else return false;
             }
         }
         return true;
