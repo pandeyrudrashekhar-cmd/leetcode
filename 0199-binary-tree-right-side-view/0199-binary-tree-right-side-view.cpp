a86f98f6adafc1abb17b41dcf13a6840483d239b
Rudra shekhar Pandey <pandeyrudrashekhar@gmail.com>
2026-09-10 01:08:47 +0530

    Time: 0 ms (100%), Space: 14.9 MB (85.63%) - LeetHub

---
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -0,0 +1,23 @@
+
+class Solution {
+public:
+int levels(TreeNode* root){
+    if(root==NULL) return 0;
+    return 1+max(levels(root->left),levels(root->right));
+
+}
+
+void preorder(TreeNode* root, vector<int> &ans, int level){
+    if(root==NULL) return;
+    ans[level]=root->val;
+    preorder(root->left,ans,level+1);
+    preorder(root->right,ans,level+1);
+
+}
+
+    vector<int> rightSideView(TreeNode* root) {
+      vector<int>ans(levels(root),0);
+      preorder(root,ans,0);
+      return ans;  
+    }
+};
